aaf9059848b3434bcd5c92a3cb0b4e297d582494
root <ccao519@yahoo.com>
2021-12-15 06:08:39 +0000

    remove a test python file

--- models/test.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-import pandas as pd
-engine = create_engine('sqlite:///../data/DisasterResponse.db')
-df = pd.read_sql_table('DisasterResponse', con=engine)
-print(df.head())
-
-# engine = create_engine('sqlite:///' + database_filepath)
-# table_name = os.path.basename(database_filepath).split('.')[0]
-# df = pd.read_sql_table(table_name,con=engine)
